d99d082ab12291a73e41a6308d1b5d240bd7de2d
jacksmithinsulander <666.jack.smith@protonmail.com>
2025-09-06 14:13:21 +0200

    natspec

--- packages/foundry/contracts/ChUSD.sol
+++ packages/foundry/contracts/ChUSD.sol
@@ -58,8 +58,7 @@ contract ChUSD is ERC20, Ownable {
     //   ___/ / /_/ /_/ / /_/  __(__  )
     //  /____/\__/\__,_/\__/\___/____/
 
-    // Address of the manager contract, that will be responsible of minting
-    // liquidations, etc etc
+    /// @notice Address of the manager contract responsible for minting, liquidations, etc
     address public manager;
 
     //     ______                 __                  __
@@ -69,34 +68,75 @@ contract ChUSD is ERC20, Ownable {
     //  \____/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/
 
     /**
-     * @notice
-     *  Constructor for ChUSD contract
-     *
+     * @notice Constructor for ChUSD contract
+     * @dev Initializes the contract and sets the deployer as the initial owner
      */
     constructor() {
+        // Initialize the owner to the deployer, only temporary
+        // since we will set the manager contract as the owner later
+        // in the same deployment script
         _initializeOwner(msg.sender);
     }
 
+    /**
+     * @notice 
+     *  Sets the manager contract address and transfers ownership to it
+     * @param _manager The address of the manager contract
+     *
+     */
     function setManager(address _manager) external onlyOwner {
         // Set the manager
         manager = _manager;
-        // Make it the ower
+        // Make it the owner
         transferOwnership(_manager);
     }
 
-    function name() public view override returns (string memory _name) {
+    /**
+     * @notice
+     *  Returns the name of the token
+     *
+     * @return _name The name of the token
+     *
+     */
+    function name() public pure override returns (string memory _name) {
+        // Return the name of the token
         _name = "ChUSD";
     }
 
-    function symbol() public view override returns (string memory _symbol) {
+    /**
+     * @notice
+     *  Returns the symbol of the token
+     *
+     * @return _symbol The symbol of the token
+     *
+     */
+    function symbol() public pure override returns (string memory _symbol) {
+        // Return the symbol of the token
         _symbol = "ChUSD";
     }
 
+    /**
+     * @notice
+     *  Mints new tokens to the specified address
+     *
+     * @param _to The address to mint tokens to
+     *
+     */
     function mint(address _to, uint _amount) public onlyOwner {
+        // Mint the tokens to the specified address
         _mint(_to, _amount);
     }
 
+    /**
+     * @notice
+     *  Burns tokens from the specified address
+     *
+     * @param _from The address to burn tokens from
+     * @param _amount The amount of tokens to burn
+     *
+     */
     function burn(address _from, uint _amount) public onlyOwner {
+        // Burn the tokens from the specified address
         _burn(_from, _amount);
     }
 }
--- packages/foundry/contracts/Manager.sol
+++ packages/foundry/contracts/Manager.sol
@@ -40,80 +40,213 @@ contract Manager is MainDemoConsumerBase {
     // Mapping to keep track of users mints
     mapping(address user => uint256 minted) public mintOf;
 
+    //     ______                 __                  __
+    //    / ____/___  ____  _____/ /________  _______/ /_____  _____
+    //   / /   / __ \/ __ \/ ___/ __/ ___/ / / / ___/ __/ __ \/ ___/
+    //  / /___/ /_/ / / / (__  ) /_/ /  / /_/ / /__/ /_/ /_/ / /
+    //  \____/\____/_/ /_/____/\__/_/   \__,_/\___/\__/\____/_/
+
+    /**
+     * @notice
+     *  Constructor for Manager contract
+     *
+     * @param _chUsd The address of the ChUSD token contract
+     * @param _weth The address of the WETH contract
+     * @param _oracle The address of the price oracle
+     *
+     */
     constructor(address _chUsd, address payable _weth, address _oracle) {
+        // Set the ChUSD contract instance
         chUsd = ChUSD(_chUsd);
+        // Set the WETH contract instance
         weth = WETH(_weth);
+        // Set the oracle address
         oracle = _oracle;
     }
 
-    function depositAndMint(uint256 _amount) public payable {
+    //     ____                    __    __        ______                 __  _
+    //    / __ \____ ___  ______ _/ /_  / /__     / ____/_  ______  _____/ /_(_)___  ____  _____
+    //   / /_/ / __ `/ / / / __ `/ __ \/ / _ \   / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  / ____/ /_/ / /_/ / /_/ / /_/ / /  __/  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    // /_/    \__,_/\__, /\__,_/_.___/_/\___/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+    //
+
+    /**
+     * @notice
+     *  Deposits ETH as collateral and mints ChUSD in one transaction
+     *
+     * @param _amount The amount of ChUSD to mint
+     *
+     */
+    function depositAndMint(uint256 _amount) external payable {
+        // Deposit the sent ETH as collateral
         _deposit(msg.value, msg.sender);
+        // Mint the requested amount of ChUSD
         mint(_amount);
     }
 
-    function deposit() public payable {
+    /**
+     * @notice
+     *  Deposits ETH as collateral without minting ChUSD
+     *
+     */
+    function deposit() external payable {
+        // Deposit the sent ETH as collateral
         _deposit(msg.value, msg.sender);
     }
 
+    /**
+     * @notice
+     *  Internal function to deposit ETH and update user's deposit balance
+     *
+     * @param _value The amount of ETH to deposit
+     * @param _sender The address of the user depositing
+     *
+     */
     function _deposit(uint256 _value, address _sender) internal {
+        // Convert ETH to WETH
         weth.deposit{value: _value};
+        // Update user's deposit balance
         depositOf[_sender] += _value;
     }
 
-    function burn(uint256 _amount) public {
+    //     ______     __                        __   ______                 __  _
+    //    / ____/  __/ /____  _________  ____ _/ /  / ____/_  ______  _____/ /_(_)___  ____  _____
+    //   / __/ | |/_/ __/ _ \/ ___/ __ \/ __ `/ /  / /_  / / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+    //  / /____>  </ /_/  __/ /  / / / / /_/ / /  / __/ / /_/ / / / / /__/ /_/ / /_/ / / / (__  )
+    // /_____/_/|_|\__/\___/_/  /_/ /_/\__,_/_/  /_/    \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/
+
+    /**
+     * @notice
+     *  Burns ChUSD tokens and updates user's minted balance
+     *
+     * @param _amount The amount of ChUSD to burn
+     *
+     */
+    function burn(uint256 _amount) external {
+        // Update user's minted balance
         mintOf[msg.sender] -= _amount;
+        // Burn the ChUSD tokens
         chUsd.burn(msg.sender, _amount);
     }
 
+    /**
+     * @notice
+     *  Mints ChUSD tokens if collateral ratio is sufficient
+     *
+     * @param _amount The amount of ChUSD to mint
+     *
+     */
     function mint(uint256 _amount) public {
+        // Update user's minted balance
         mintOf[msg.sender] += _amount;
+        // Check if collateral ratio is sufficient
         if (collateralRatio(msg.sender) < MIN_COLLATERAL_RATIO)
             revert CUErrors.TOO_LOW_COLLATERAL_RATIO();
+        // Mint the ChUSD tokens
         chUsd.mint(msg.sender, _amount);
     }
 
-    function withdraw(uint256 _amount) public {
+    /**
+     * @notice
+     *  Withdraws ETH collateral if collateral ratio remains sufficient
+     *
+     * @param _amount The amount of ETH to withdraw
+     *
+     */
+    function withdraw(uint256 _amount) external {
+        // Update user's deposit balance
         depositOf[msg.sender] -= _amount;
+        // Check if collateral ratio is still sufficient
         if (collateralRatio(msg.sender) < MIN_COLLATERAL_RATIO)
             revert CUErrors.TOO_LOW_COLLATERAL_RATIO();
+        // Convert WETH back to ETH
         weth.withdraw(_amount);
+        // Transfer ETH to user
         msg.sender.safeTransferETH(_amount);
     }
 
-    function liquidate(address _user) public {
+    /**
+     * @notice
+     *  Liquidates a user with insufficient collateral ratio
+     *
+     * @param _user The address of the user to liquidate
+     *
+     */
+    function liquidate(address _user) external {
+        // Get the user's current collateral ratio
         uint256 userCollateralRatio = collateralRatio(_user);
+        // Check if user can be liquidated
         if (userCollateralRatio > MIN_COLLATERAL_RATIO)
             revert CUErrors.CANT_LIQUIDATE_USER(_user, userCollateralRatio);
+        // Burn the user's minted ChUSD
         chUsd.burn(msg.sender, mintOf[_user]);
+        // Convert user's WETH to ETH
         weth.withdraw(depositOf[_user]);
+        // Transfer the ETH to the liquidator
         msg.sender.safeTransferETH(depositOf[_user]);
+        // Reset user's balances
         depositOf[_user] = 0;
         mintOf[_user] = 0;
     }
 
-    function collateralRatio(
-        address _user
-    ) public view returns (uint256 _ratio) {
-        _ratio = _collateralRatio(mintOf[_user], depositOf[_user]);
-    }
-
+    /**
+     * @notice
+     *  Returns a quote for the collateral ratio if additional deposit is made
+     *
+     * @param _user The address of the user to quote for
+     * @param _addedDeposit The additional deposit amount to consider
+     *
+     * @return _quoteRatio The projected collateral ratio
+     *
+     */
     function quote(
         address _user,
         uint256 _addedDeposit
     ) external view returns (uint256 _quoteRatio) {
+        // Calculate the collateral ratio with the additional deposit
         _quoteRatio = _collateralRatio(
             (mintOf[_user] + _addedDeposit),
             depositOf[_user]
         );
     }
 
+    /**
+     * @notice
+     *  Returns the collateral ratio for a given user
+     *
+     * @param _user The address of the user to check
+     *
+     * @return _ratio The collateral ratio (deposited value / minted value)
+     *
+     */
+    function collateralRatio(
+        address _user
+    ) public view returns (uint256 _ratio) {
+        // Calculate and return the collateral ratio
+        _ratio = _collateralRatio(mintOf[_user], depositOf[_user]);
+    }
+
+    /**
+     * @notice
+     *  Internal function to calculate collateral ratio using oracle price
+     *
+     * @param _minted The amount of ChUSD minted
+     * @param _deposited The amount of WETH deposited
+     *
+     * @return _ratio The calculated collateral ratio
+     *
+     */
     function _collateralRatio(
         uint256 _minted,
         uint256 _deposited
     ) internal view returns (uint256 _ratio) {
+        // If no ChUSD minted, return max ratio
         if (_minted == 0) return type(uint256).max;
+        // Calculate total value using oracle price
         uint256 totalValue = (_deposited *
             (getOracleNumericValueFromTxMsg(bytes32("ETH")) * 1e10)) / 1e18;
+        // Return the ratio
         _ratio = totalValue / _minted;
     }
 }
